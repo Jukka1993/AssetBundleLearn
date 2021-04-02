a3d7705b5b5cc21c5a216692735167c1897e25e6
jukka <jukka@DESKTOP-K8LQ3VJ>
2021-04-03 02:27:00 +0800

    assetbundles pack and load use

---
+++ AssetBundleLearn/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    public static void BuildAllAssetBundles()
+    {
+        string output = "AssetBundles";
+        if (!Directory.Exists(output))
+        {
+            Directory.CreateDirectory(output);
+        }
+        BuildPipeline.BuildAssetBundles(output,BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+
+    }
+}
---
+++ AssetBundleLearn/Assets/Scripts/LoadFromFile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadFromFile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/scene/ball.unity3d");
+        
+        string[] names = ab.GetAllAssetNames();
+        
+        Debug.Log(names);
+        foreach (string name in names)
+        {
+            Debug.Log(name);
+        }
+        GameObject o = ab.LoadAsset<GameObject>("ball");
+        Instantiate(o);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
